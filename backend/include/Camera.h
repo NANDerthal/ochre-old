86aefac426da9349ca68a44f578f41d668c70bdb
Sunny <quatridunull@gmail.com>
2016-07-26 15:33:49 -0600

    Fix matrix math and add camera movement functionality

--- backend/include/Camera.h
+++ backend/include/Camera.h
@@ -3,22 +3,32 @@
 
 #include <glm/fwd.hpp>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 class Camera {
 
 private:
 
 	glm::mat4 cameraMatrix;
-	bool matrixIsValid;
+	glm::mat3 normalize;
+	glm::vec3 position, target, up;
+	float fieldOfView; // degrees
+	bool matrixIsValid, usePerspective;
 
 public:
 
 	// ========== constructors and destructors ==========
 
-	Camera();
+	Camera( const glm::mat3 &normalizeIn );
 	~Camera();
 
 	// ========== get and set member variables ==========
+
+	void setPosition( const glm::vec3 &positionIn );
+	void setTarget( const glm::vec3 &targetIn );
+	void setUp( const glm::vec3 &upIn );
+	void setFieldOfView( const float fov );
+	void setUsePerpective( const bool usePerspectiveIn );
 
 	glm::mat4 getMatrix();
 
--- backend/lib/Camera.cpp
+++ backend/lib/Camera.cpp
@@ -1,10 +1,16 @@
 #include "Camera.h"
+#include <cstdio>
 
 // ========== constructors and destructors ==========
 
-Camera::Camera() {
-	cameraMatrix = glm::mat4( 1.0f );
+Camera::Camera( const glm::mat3 &normalizeIn ) {
+	normalize = normalizeIn;
+	position = glm::vec3( 0.0f, 0.0f, 0.0f );
+	target = glm::vec3( 0.0f, 0.0f, 1.0f );
+	up = glm::vec3( 0.0f, 1.0f, 0.0f );
+	fieldOfView = 90.0f; // pixel-perfect 2D at the camera's image plane
 	matrixIsValid = false;
+	usePerspective = true;
 	return;
 } // Camera
 
@@ -14,11 +20,62 @@ Camera::~Camera() {
 
 // ========== get and set member variables ==========
 
+void Camera::setPosition( const glm::vec3 &positionIn ) {
+	position = positionIn;
+	matrixIsValid = false;
+	return;
+} // setPosition
+
+void Camera::setTarget( const glm::vec3 &targetIn ) {
+	target = targetIn;
+	matrixIsValid = false;
+	return;
+} // setTarget
+
+void Camera::setUp( const glm::vec3 &upIn ) {
+	up = upIn;
+	matrixIsValid = false;
+	return;
+} // setUp
+
+void Camera::setFieldOfView( const float fov ) {
+	fieldOfView = fov;
+	matrixIsValid = false;
+	return;
+} // setFieldOfView
+
+void Camera::setUsePerpective( const bool usePerspectiveIn ) {
+	usePerspective = usePerspectiveIn;
+	matrixIsValid = false;
+	return;
+} // setUsePerspective
+
 glm::mat4 Camera::getMatrix() {
-	if ( matrixIsValid ) {
-		return cameraMatrix;
-	} else {
-		// TODO: calculate matrix based on camera parameters
+	if ( !matrixIsValid ) {
+		// camera position
+		// position, target, up
+		// note that the target coordinate z-dimension is the opposite direction of
+		// where you actually want to look - suppose you have z axis <-- and
+		// object ------> camera
+		// and want to look at the object. Then target should be in the --> direction
+		// likewise, the position z-coord is negative of the world z-coord:
+		// <---world positive z-direction; --->camera positive z-direction
+		cameraMatrix = glm::lookAt( normalize * position, 
+									glm::vec3( target.x, target.y, -target.z ), 
+									normalize * up );
+
+		if ( usePerspective ) {
+			// perspective distortion
+			glm::mat4 perspective = glm::perspective( glm::radians( fieldOfView ),
+													  1.0f, // aspect ratio
+													  0.0f, 1000.0f ); // clipping planes
+			// correct glm incompatibility (glm does not add 1 * w-coordinate for perspective)
+			perspective[3][3] = 1;
+
+			cameraMatrix = perspective * cameraMatrix;
+		}
+
+		matrixIsValid = true;
 	}
 
 	return cameraMatrix;
